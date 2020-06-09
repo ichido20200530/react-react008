3ced002b45c4ef455be544ae499118954f460053
ichido <ichido@live.jp>
2020-06-10 07:33:48 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  input = '';
+
+  msgStyle = {
+    fontSize:"20pt",
+    color:"#900",
+    margin:"20px 0px",
+    padding: "5px",
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'type your name:'
+    };
+    this.doChange = this.doChange.bind(this);
+    this.doSubmit = this.doSubmit.bind(this);
+  }
+
+  doChange(event) {
+    this.input = event.target.value;
+  }
+
+  doSubmit(event) {
+    this.setState({
+      message: 'Hello, ' + this.input + '!!'
+    });
+    event.preventDefault();
+  }
+
+  render() {
+    return <div>
+      <h1>React</h1>
+      <Message title="Children!">
+        これはコンポーネント内のコンテンツです。
+        マルでテキストを分割し、リストにして表示します。
+        改行は必要ありません。
+      </Message>
+    </div>;
+  }
 }
 
-export default App;
+class Message extends Component {
+  li = {
+    fontSize: "16pt",
+    color: "#06",
+    margin: "0px",
+    padding: "0px",
+  }
+
+  render(){
+    let content = this.props.children;
+    let arr = content.split('。');
+    let arr2 = [];
+    for( let i = 0; i < arr.length; i++ ) {
+      if ( arr[i].trim() !== '') {
+        arr2.push(arr[i]);
+      }
+    }
+    let list = arr2.map((value,key) => (
+      <li style={this.li} key={key}>{value}.</li>
+    ));
+
+    return <div>
+      <h2>{this.props.title}</h2>
+      <ol>{list}</ol>
+    </div>
+  }
+}
